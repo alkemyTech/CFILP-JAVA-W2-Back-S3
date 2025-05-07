a287d9b405627e537ef2ba2282c05b34fee887ed
Patricio Veras Carrara (Pato 🦆) <137646422+patoveras7@users.noreply.github.com>
2025-05-06 22:33:44 -0300

    Update tables.sql
    Some changes in DB.

--- DB/tables.sql
+++ DB/tables.sql
@@ -1,26 +1,26 @@
 
 CREATE TABLE Usuario (
-  id              INTEGER PRIMARY KEY AUTOINCREMENT,
+  id_usuario      INTEGER PRIMARY KEY AUTOINCREMENT,
   nombre          TEXT NOT NULL,
   apellido        TEXT NOT NULL,
   email           TEXT NOT NULL UNIQUE,
   telefono        TEXT,
   fecha_creacion  TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  contrasena      TEXT NOT NULL
+  contrasenia     TEXT NOT NULL
 );
 
 CREATE TABLE Cliente (
-  id              INTEGER PRIMARY KEY,
-  FOREIGN KEY (id) REFERENCES Usuario(id)
+  id_cliente      INTEGER PRIMARY KEY,
+  FOREIGN KEY (id_cliente) REFERENCES Usuario(id_usuario)
 );
 
 CREATE TABLE Admin (
-  id              INTEGER PRIMARY KEY,
-  FOREIGN KEY (id) REFERENCES Usuario(id)
+  id_admin        INTEGER PRIMARY KEY,
+  FOREIGN KEY (id_admin) REFERENCES Usuario(id_usuario)
 );
 
 CREATE TABLE Cuenta (
-  nro_cuenta      TEXT PRIMARY KEY,
+  numero_cuenta   TEXT PRIMARY KEY,
   moneda          TEXT NOT NULL,
   monto           REAL NOT NULL DEFAULT 0.0,
   fecha           TEXT NOT NULL,
@@ -28,26 +28,26 @@ CREATE TABLE Cuenta (
   tipo            TEXT NOT NULL,
   cvu             TEXT UNIQUE NOT NULL,
   cliente_id      INTEGER NOT NULL,
-  FOREIGN KEY (cliente_id) REFERENCES Cliente(id)
+  FOREIGN KEY (cliente_id) REFERENCES Cliente(id_cliente)
 );
 
 CREATE TABLE Tarjeta (
-  nro_tarjeta       TEXT PRIMARY KEY,
-  cod_sec           TEXT NOT NULL,
+  numero_tarjeta    TEXT PRIMARY KEY,
+  codigo_seguridad  TEXT NOT NULL,
   fecha_vencimiento TEXT NOT NULL,
   compania          TEXT NOT NULL,
   tipo              TEXT NOT NULL,
   fecha_emision     TEXT NOT NULL,
   particular        TEXT NOT NULL,
-  propia            INTEGER NOT NULL DEFAULT 1 -- Indica si es dada por nosotros o por otra compañia como VISA Brubank
+  propia            INTEGER NOT NULL DEFAULT 1 -- Indica si es dada por nosotros o por otra compañia como, VISA, Brubank, etc. Si es 0 es FALSE y si es 1 es TRUE
 );
 
 CREATE TABLE Cuenta_Tarjeta (
-  nro_cuenta    TEXT NOT NULL,
-  nro_tarjeta   TEXT NOT NULL,
-  PRIMARY KEY (nro_cuenta, nro_tarjeta),
-  FOREIGN KEY (nro_cuenta) REFERENCES Cuenta(nro_cuenta),
-  FOREIGN KEY (nro_tarjeta) REFERENCES Tarjeta(nro_tarjeta)
+  numero_cuenta    TEXT NOT NULL,
+  numero_tarjeta   TEXT NOT NULL,
+  PRIMARY KEY (numero_cuenta, numero_tarjeta),
+  FOREIGN KEY (numero_cuenta) REFERENCES Cuenta(numero_cuenta),
+  FOREIGN KEY (numero_tarjeta) REFERENCES Tarjeta(numero_tarjeta)
 );
 
 CREATE TABLE Transaccion (
@@ -59,26 +59,26 @@ CREATE TABLE Transaccion (
 );
 
 CREATE TABLE Deposito (
-  id_transaccion  INTEGER PRIMARY KEY,
+  id_deposito     INTEGER PRIMARY KEY,
   cuenta_destino  TEXT NOT NULL,
-  FOREIGN KEY (id_transaccion) REFERENCES Transaccion(id_transaccion),
-  FOREIGN KEY (cuenta_destino) REFERENCES Cuenta(nro_cuenta)
+  FOREIGN KEY (id_deposito) REFERENCES Transaccion(id_transaccion),
+  FOREIGN KEY (cuenta_destino) REFERENCES Cuenta(numero_cuenta)
 );
 
 CREATE TABLE Extraccion (
-  id_transaccion  INTEGER PRIMARY KEY,
+  id_extraccion  INTEGER PRIMARY KEY,
   cuenta_origen   TEXT NOT NULL,
-  FOREIGN KEY (id_transaccion) REFERENCES Transaccion(id_transaccion),
-  FOREIGN KEY (cuenta_origen) REFERENCES Cuenta(nro_cuenta)
+  FOREIGN KEY (id_extraccion) REFERENCES Transaccion(id_transaccion),
+  FOREIGN KEY (cuenta_origen) REFERENCES Cuenta(numero_cuenta)
 );
 
 CREATE TABLE Transferencia (
-  id_transaccion  INTEGER PRIMARY KEY,
-  cuenta_origen   TEXT NOT NULL,
-  cuenta_destino  TEXT NOT NULL,
-  FOREIGN KEY (id_transaccion) REFERENCES Transaccion(id_transaccion),
-  FOREIGN KEY (cuenta_origen)  REFERENCES Cuenta(nro_cuenta),
-  FOREIGN KEY (cuenta_destino) REFERENCES Cuenta(nro_cuenta)
+  id_transferencia INTEGER PRIMARY KEY,
+  cuenta_origen    TEXT NOT NULL,
+  cuenta_destino   TEXT NOT NULL,
+  FOREIGN KEY (id_transferencia) REFERENCES Transaccion(id_transaccion),
+  FOREIGN KEY (cuenta_origen)  REFERENCES Cuenta(numero_cuenta),
+  FOREIGN KEY (cuenta_destino) REFERENCES Cuenta(numero_cuenta)
 );
 
 
