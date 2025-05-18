643225428670456d640e56c0e1d5c4574e8c73ab
Patricio Veras Carrara (Pato 🦆) <137646422+patoveras7@users.noreply.github.com>
2025-05-18 13:16:59 -0300

    Update tables.sql

--- DB/tables.sql
+++ DB/tables.sql
@@ -29,7 +29,7 @@ CREATE TABLE Tarjeta (
   tipo              TEXT NOT NULL,
   fecha_emision     DATE NOT NULL,
   particular        TEXT NOT NULL,
-  propia            TEXT NOT NULL DEFAULT 'PROPIA' CHECK (rol IN ('PROPIA', 'TERCERO')) 
+  propia            TEXT NOT NULL DEFAULT 'PROPIA' CHECK (propia IN ('PROPIA', 'TERCERO')) 
 );
 
 CREATE TABLE Cuenta_Tarjeta (
