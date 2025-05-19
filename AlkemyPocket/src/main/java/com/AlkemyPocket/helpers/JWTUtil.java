1acfddd2f1b602068c6d2c9055f6fe84a4b4ffc9
patoveras7 <patricioverasc@gmail.com>
2025-05-19 18:59:24 -0300

    LOGIN CON BCRIPT Y JWT => OK

---
+++ AlkemyPocket/src/main/java/com/AlkemyPocket/helpers/JWTUtil.java
@@ -0,0 +1,23 @@
+package com.AlkemyPocket.helpers;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private static final String SECRET_KEY = "MiClaveSecretaSuperSegura123"; // Podés mover esto a application.properties
+    private static final long EXPIRATION_TIME = 86400000; // 24 hs
+
+    public String generarToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
